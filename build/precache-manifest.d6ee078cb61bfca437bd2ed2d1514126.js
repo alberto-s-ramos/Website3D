ebc73e901ee94ae05aaf42f878f48987f412f680
albertoramosTD <alberto.ramos@talkdesk.com>
2021-08-04 09:51:35 +0100

    fix

---
+++ build/precache-manifest.d6ee078cb61bfca437bd2ed2d1514126.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87fa73fd38240c380d56c439f52dd89a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3540cee096b3b810e228",
+    "url": "/static/css/2.22ca87da.chunk.css"
+  },
+  {
+    "revision": "824d01d2c931e26b8a6c",
+    "url": "/static/css/main.54c9f2ce.chunk.css"
+  },
+  {
+    "revision": "3540cee096b3b810e228",
+    "url": "/static/js/2.d54090ad.chunk.js"
+  },
+  {
+    "revision": "bbf7c6b975aac15aaee7bf56eee0ba7f",
+    "url": "/static/js/2.d54090ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "824d01d2c931e26b8a6c",
+    "url": "/static/js/main.3ebef39e.chunk.js"
+  },
+  {
+    "revision": "142b697ed77b435eaccc",
+    "url": "/static/js/runtime-main.efa8e349.js"
+  }
+]);
