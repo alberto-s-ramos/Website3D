135a672b4a815607db1c91f613f99c644f33ee28
albertoramosTD <alberto.ramos@talkdesk.com>
2021-08-04 13:42:24 +0100

    add cv

---
+++ build/precache-manifest.4c1c098a0a5da28efcff05fe0745a0d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d13119e33b683b3db0c9888b88c9f6d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "870c1c72d59757be82ad",
+    "url": "/static/css/2.22ca87da.chunk.css"
+  },
+  {
+    "revision": "4ead75e364ff3721dae6",
+    "url": "/static/css/main.6d94d3b2.chunk.css"
+  },
+  {
+    "revision": "870c1c72d59757be82ad",
+    "url": "/static/js/2.926c0f68.chunk.js"
+  },
+  {
+    "revision": "bbf7c6b975aac15aaee7bf56eee0ba7f",
+    "url": "/static/js/2.926c0f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ead75e364ff3721dae6",
+    "url": "/static/js/main.deaa03f5.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65e50cf4b3fa6dbb521f561827e82194.js"
+  "/precache-manifest.4c1c098a0a5da28efcff05fe0745a0d4.js"
 );
 
 self.addEventListener('message', (event) => {
